fb5a347edc11477a9c39b560139c0cbb58eb3d22
Justin Peacock <justin@sixteenbit.com>
2014-11-06 13:39:25 -0500

    fixed fluid and sidebar

--- root/sidebar.php
+++ root/sidebar.php
@@ -10,6 +10,8 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+<div id="secondary" class="site-sidebar" role="complementary">
+	<div class="widgets-area">
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	</div>
 </div><!-- #secondary -->
