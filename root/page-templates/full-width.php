2a5b703a17792d528faabd8e1957d0ca7f83877b
Justin Peacock <justin@sixteenbit.com>
2014-11-11 17:39:55 -0500

    full width template fix

--- root/page-templates/full-width.php
+++ root/page-templates/full-width.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 
 	<div id="content" class="site-content">
-		<div id="primary" class="content-area">
+		<div id="primary" class="content-area full-width">
 			<main id="main" class="site-main" role="main">
 
 				<?php while ( have_posts() ) : the_post(); ?>
