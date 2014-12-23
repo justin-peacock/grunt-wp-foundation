146faef3a8f276e2c62cdfdf48fc4ba26629de65
Justin Peacock <justin@sixteenbit.com>
2014-12-23 11:05:40 -0500

    * changed font awesome to an @extend * added icons for meta and moved to footer of post

--- root/inc/template-tags.php
+++ root/inc/template-tags.php
@@ -106,11 +106,11 @@ function {%= prefix %}_post_nav() {
 }
 endif;
 
-if ( ! function_exists( '{%= prefix %}_posted_on' ) ) :
+if ( ! function_exists( '{%= prefix %}_entry_footer' ) ) :
 /**
- * Prints HTML with meta information for the current post-date/time and author.
+ * Prints HTML with meta information for the categories, tags and comments.
  */
-function {%= prefix %}_posted_on() {
+function {%= prefix %}_entry_footer() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
@@ -124,7 +124,7 @@ function {%= prefix %}_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		_x( 'Posted on %s', 'post date', '{%= prefix %}' ),
+		_x( '%s', 'post date', '{%= prefix %}' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
@@ -135,33 +135,19 @@ function {%= prefix %}_posted_on() {
 
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
 
-}
-endif;
-
-if ( ! function_exists( '{%= prefix %}_entry_footer' ) ) :
-/**
- * Prints HTML with meta information for the categories, tags and comments.
- */
-function {%= prefix %}_entry_footer() {
 	// Hide category and tag text for pages.
 	if ( 'post' == get_post_type() ) {
 		/* translators: used between list items, there is a space after the comma */
 		$categories_list = get_the_category_list( __( ', ', '{%= prefix %}' ) );
 		if ( $categories_list && {%= prefix %}_categorized_blog() ) {
-			printf( '<span class="cat-links">' . __( 'Posted in %1$s', '{%= prefix %}' ) . '</span>', $categories_list );
+			printf( '<span class="cat-links">' . __( '%1$s', '{%= prefix %}' ) . '</span>', $categories_list );
 		}
 
 		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '', __( ', ', '{%= prefix %}' ) );
 		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . __( 'Tagged %1$s', '{%= prefix %}' ) . '</span>', $tags_list );
+			printf( '<span class="tags-links">' . __( '%1$s', '{%= prefix %}' ) . '</span>', $tags_list );
 		}
-	}
-
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		comments_popup_link( __( 'Leave a comment', '{%= prefix %}' ), __( '1 Comment', '{%= prefix %}' ), __( '% Comments', '{%= prefix %}' ) );
-		echo '</span>';
 	}
 
 	edit_post_link( __( 'Edit', '{%= prefix %}' ), '<span class="edit-link">', '</span>' );
